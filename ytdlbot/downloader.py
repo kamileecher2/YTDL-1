7f94fc5c5fbf5d512eb7eaf65115e1d4795d0c87
kamileecher2 <91373907+kamileecher2@users.noreply.github.com>
2022-02-19 10:52:46 +0300

    Update downloader.py

--- ytdlbot/downloader.py
+++ ytdlbot/downloader.py
@@ -35,7 +35,7 @@ apply_log_formatter()
 
 
 def sizeof_fmt(num: int, suffix='B'):
-    for unit in ['', 'Kb', 'Mb', 'Gb', 'Tb', 'Pb', 'Eb', 'Zb']:
+    for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
         if abs(num) < 1024.0:
             return "%3.1f%s%s" % (num, unit, suffix)
         num /= 1024.0
